a2c1a42858261960ec2f2fffd3ef3adef0c60416
karansaratha@gmail.com <karansaratha@gmail.com>
2021-09-22 18:18:47 +0530

    Initial Commit

---
+++ Koodai.comTests/Koodai_comTests.swift
@@ -0,0 +1,33 @@
+//
+//  Koodai_comTests.swift
+//  Koodai.comTests
+//
+//  Created by HappysanziMac on 22/09/21.
+//
+
+import XCTest
+@testable import Koodai_com
+
+class Koodai_comTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
